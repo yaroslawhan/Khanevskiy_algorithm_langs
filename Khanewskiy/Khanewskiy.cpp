d51418b3707acee50d8ff5abdeab5aeb8287b7cb
Han <yaroslav.khanevskiy@gmail.com>
2024-09-19 22:28:47 +0400

    Add some new functional, modified "add a pipe" and "view all objects" options.

--- Khanewskiy/Khanewskiy.cpp
+++ Khanewskiy/Khanewskiy.cpp
@@ -1,13 +1,15 @@
-﻿// Khanewskiy.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿/* TODO
+* 1. line 43. Error on reading float numbers like 123.456.789 and 123.2343wewe
+*/
 
 #include <iostream>
+#include <fstream>
 
 struct Pipe
 {
 	std::string name;
-	double length;
-	double diameter;
+	float length;
+	float diameter;
 	bool isRepairing = false;
 };
 
@@ -16,16 +18,10 @@ struct CompressorStation
 	std::string name;
 	unsigned short int workshopNum;
 	unsigned short int activeWorkshopNum;
-	double effectiveness;
+	float effectiveness;
 };
 
-std::string action; // Action in menu
-bool programFlag = true;
-Pipe firstPipe;
-CompressorStation cs;
-std::string temp;
-
-void createMenu() {
+void showMenu() {
 	std::cout << "Gas pipeline transport model \n\n";
 	std::cout << "Menu: \n";
 	std::cout << "1. Add a pipe \n";
@@ -39,44 +35,278 @@ void createMenu() {
 	std::cout << "Please select an action :";
 }
 
-Pipe addPipe() {
-	Pipe pipe;
+void addPipe(Pipe &pipe) {
 	std::cout << "Enter name(mileage) of the pipe: ";
 	std::cin >> pipe.name;
 
 	while (true) {
 		std::cout << "\nEnter pipe length (in meters): ";
-		std::cin >> temp;
-		std::cout << std::atof(temp.c_str());
-		if (std::atof(temp.c_str()) == 0) std::cout << "ERROR";
+		std::cin >> pipe.length;
+
+		if (std::cin.fail()) {
+			system("cls");
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "ERROR\n";
+		}
+		else if (pipe.length <= 0) {
+			system("cls");
+			std::cout << "ERROR. Pipe length can`t be less than 0 meters or be equal 0 meters\n";
+		}
+		else if (pipe.length > 50) {
+			system("cls");
+			std::cout << "ERROR. Pipe length can`t be more than 50 meters\n";
+		}
 		else {
-			pipe.length = std::atof(temp.c_str());
 			break;
 		}
 	}
 
-	std::cout << "\nEnter pipe diameter (in millimeters): ";
-	std::cin >> pipe.diameter;
-	std::cout << "\nChoose pipe status (0 - in repairing, 1 - not in repairing): ";
-	std::cin >> pipe.isRepairing;
-	return pipe;
+	while (true) {
+		std::cout << "\nEnter pipe diameter (in millimeters): ";
+		std::cin >> pipe.diameter;
+
+		if (std::cin.fail()) {
+			system("cls");
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "ERROR\n";
+		}
+		else if (pipe.diameter <= 0) {
+			system("cls");
+			std::cout << "ERROR. Pipe diameter can`t be less than 0 millimeters or be equal 0 millimeters\n";
+		}
+		else if (pipe.diameter > 5000) {
+			system("cls");
+			std::cout << "ERROR. Pipe diameter can`t be more than 5000 millimeters\n";
+		}
+		else {
+			break;
+		}
+	}
+
+	while (true) {
+		std::cout << "\nChoose pipe status (0 - in repairing, 1 - not in repairing): ";
+		std::cin >> pipe.isRepairing;
+
+		if (std::cin.fail()) {
+			system("cls");
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "ERROR\n";
+		}
+		else {
+			break;
+		}
+	}
 }
 
-void showPipe(Pipe pipe) {
+void addCs(CompressorStation& cs) {
+	std::cout << "Enter name of the compressor station: ";
+	std::cin >> cs.name;
+
+	while (true) {
+		std::cout << "\nEnter the number of workshops: ";
+		std::cin >> cs.workshopNum;
+
+		if (std::cin.fail()) {
+			system("cls");
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "ERROR\n";
+		}
+		else if (cs.workshopNum <= 0) {
+			system("cls");
+			std::cout << "ERROR. The number of workshops can`t be less than 0 or be equal 0\n";
+		}
+		else if (cs.workshopNum > 100) {
+			system("cls");
+			std::cout << "ERROR. The number of workshops can`t be more than 100\n";
+		}
+		else {
+			break;
+		}
+	}
+
+	while (true) {
+		std::cout << "\nEnter the number of active workshops: ";
+		std::cin >> cs.activeWorkshopNum;
+
+		if (std::cin.fail()) {
+			system("cls");
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "ERROR\n";
+		}
+		else if (cs.activeWorkshopNum < 0) {
+			system("cls");
+			std::cout << "ERROR. The number of active workshops can`t be less than 0\n";
+		}
+		else if (cs.activeWorkshopNum > 100) {
+			system("cls");
+			std::cout << "ERROR. The number of active workshops can`t be more than 100\n";
+		}
+		else if (cs.activeWorkshopNum > cs.workshopNum) {
+			system("cls");
+			std::cout << "ERROR. The number of active workshops can`t be more than the number of workshops\n";
+		}
+		else {
+			break;
+		}
+	}
+
+	while (true) {
+		std::cout << "\nEnter the compressor station effectiveness: ";
+		std::cin >> cs.effectiveness;
+
+		if (std::cin.fail()) {
+			system("cls");
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			std::cout << "ERROR\n";
+		}
+		else {
+			break;
+		}
+	}
+}
+
+void showPipes(Pipe &pipe) {
 	std::cout << "Information about pipes:\n\n";
 	std::cout << "Pipe name: " << pipe.name;
 	std::cout << "\nPipe length: " << pipe.length;
 	std::cout << "\nPipe diameter: " << pipe.diameter;
-	std::cout << "\nPipe status: " << ((pipe.isRepairing == true) ? "not in repairing\n\n" : "in repairing\n\n");
+	std::cout << "\nPipe status: " << ((pipe.isRepairing == true) ? "not in repairing\n\n\n" : "in repairing\n\n\n");
+}
+
+void showCss(CompressorStation& cs) {
+	std::cout << "Information about compressor stations:\n\n";
+	std::cout << "Compressor station name: " << cs.name;
+	std::cout << "\nThe number of workshops: " << cs.workshopNum;
+	std::cout << "\nThe number of active workshops: " << cs.activeWorkshopNum;
+	std::cout << "\nThe compressor station effectiveness: " << cs.effectiveness << "\n\n\n";
+}
+
+void save(Pipe& pipe, bool pipeExist, CompressorStation& cs, bool csExist) {
+	std::ofstream out;
+	out.open("data.txt");
+
+	if (out.is_open()) {
+		if (pipeExist) {
+			
+		}
+		
+		if (csExist) {
+
+		}
+	}
+	out.close();
 }
 
 int main()
 {
+	bool programFlag = true;
+	Pipe firstPipe;
+	Pipe &pipe = firstPipe;
+	bool pipeExist = false;
+	CompressorStation firstCs;
+	CompressorStation& cs = firstCs;
+	bool csExist = false;
+
 	while (programFlag) // Infinity cycle causes a menu
 	{
-		createMenu();
+		showMenu();
+		unsigned short int action; // Action in menu
 		std::cin >> action;
 
+		if (std::cin.good() && action >= 0 && action <= 7) {
+			switch (action) {
+			case 0:
+				programFlag = false;
+				break;
+
+			case 1:
+				system("cls");
+				if (pipeExist) {
+					std::cout << "Pipe is already exist\n\n";
+				}
+				else {
+					addPipe(pipe);
+					system("cls");
+					std::cout << "New pipe added successfully!\n\n";
+					pipeExist = true;
+				}
+				break;
+
+			case 2:
+				system("cls");
+				if (csExist) {
+					std::cout << "Compressor station is already exist\n\n";
+				}
+				else {
+					addCs(cs);
+					system("cls");
+					std::cout << "New compressor station added successfully!\n\n";
+					csExist = true;
+				}
+				break;
+			case 3:
+				system("cls");
+				if (pipeExist) {
+					showPipes(pipe);
+				}
+				else {
+					std::cout << "Pipe does not exist\n\n";
+				}
+				if (csExist) {
+					showCss(cs);
+				}
+				else {
+					std::cout << "Compressor station does not exist\n\n";
+				}
+				break;
+			case 4:
+				system("cls");
+				if (pipeExist) {
+					addPipe(pipe);
+					system("cls");
+					std::cout << "The pipe was successfully edited!\n\n";
+				}
+				else {
+					std::cout << "ERROR. Pipe does not exist\n\n";
+				}
+				break;
+			case 5:
+				system("cls");
+				if (csExist) {
+					addCs(cs);
+					system("cls");
+					std::cout << "The compressor station was successfully edited!\n\n";
+				}
+				else {
+					std::cout << "ERROR. Compressor station does not exist\n\n";
+				}
+				break;
+			case 6:
+				system("cls");
+				save(pipe, pipeExist, cs, csExist);
+				std::cout << "Information was saved!\n\n";
+				break;
+			case 7:
+				system("cls");
+				std::cout << "Coming soon\n\n";
+				break;
+			}
+		}
+		else {
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			system("cls");
+			std::cout << "ERROR\n\n";
+		}
+
+
+		/*
 		if (action == "0") {
 			programFlag = false;
 		}
@@ -94,6 +324,7 @@ int main()
 			system("cls");
 			std::cout << "ERROR\n\n";
 		}
+		*/
 	}
 	return 0;
 }
